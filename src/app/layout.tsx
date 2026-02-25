fdc3f5edc5af85b9d7f0c53199c4d13123d97039
Shafi Akinropo <sakinropo@gmail.com>
2026-02-25 07:28:04 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body >
         <Providers>{children}</Providers>
       </body>
     </html>
